417cb8a249311508a3abba75e6e3a835118f70fe
DoodleSchrank <39834655+DoodleSchrank@users.noreply.github.com>
2020-12-10 23:33:28 +0100

    Update openmp-example.cpp

--- openmp-example.cpp
+++ openmp-example.cpp
@@ -1,64 +1,70 @@
 #include <iostream>
-#include <omp.h>
 #include <chrono>
+#include <omp.h>
 
 #define vectorsize 1000000
-#define iterations 100
+#define simlength 100
 
 double A[vectorsize];
 
 void cpu() {
-    std::cout << "--- CPU ---\n";
-    auto start = std::chrono::high_resolution_clock::now();
+	std::cout << "Starting CPU computation ..." << std::endl;
 
-    #pragma omp parallel for collapse(2)
-    for(int i = 0; i < iterations; i++) {
-        for(int j = 0; j < vectorsize; j++) {
+	auto start = std::chrono::high_resolution_clock::now();
+	#pragma omp parallel for collapse(2)
+    for(int i = 0; i < simlength; i++)
+    {
+        for(int j = 0; j < vectorsize; j++)
             A[j] *= 1.1 * 0.9;
-        }
     }
-
-    auto finish = std::chrono::high_resolution_clock::now();
-    std::cout << "Elapsed time: " << (float) std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count()/1000 << " s\n";
+	auto end = std::chrono::high_resolution_clock::now();
+	
+	std::cout << std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count() << " seconds" << std::endl;
 }
 
 void simd() {
-    std::cout << "--- SIMD ---\n";
-    auto start = std::chrono::high_resolution_clock::now();
+	std::cout << "Starting SIMD computation ..." << std::endl;
 
-    #pragma omp parallel for simd collapse(2)
-    for(int i = 0; i < iterations; i++) {
-        for(int j = 0; j < vectorsize; j++) {
+	auto start = std::chrono::high_resolution_clock::now();
+	#pragma omp simd collapse(2)
+    for(int i = 0; i < simlength; i++)
+    {
+        for(int j = 0; j < vectorsize; j++)
             A[j] *= 1.1 * 0.9;
-        }
     }
+	auto end = std::chrono::high_resolution_clock::now();
 
-    auto finish = std::chrono::high_resolution_clock::now();
-    std::cout << "Elapsed time: " << (float) std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count()/1000 << " s\n";
+	std::cout << std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count() << " seconds" << std::endl;
+	std::cout << omp_get_max_threads() << " threads" <<std::endl;
 }
 
 void gpu() {
-    std::cout << "--- GPU ---\n";
-    auto start = std::chrono::high_resolution_clock::now();
+	std::cout << "Starting GPU computation ..." << std::endl;
+	std::cout << omp_get_num_devices() << " devices" << std::endl;
 
-    #pragma omp target teams distribute parallel for collapse(2)
-    for(int i = 0; i < iterations; i++) {
-        for(int j = 0; j < vectorsize; j++) {
+	auto start = std::chrono::high_resolution_clock::now();
+	#pragma omp target teams distribute \
+							parallel for map(A) collapse(2)
+    for(int i = 0; i < simlength; i++)
+    {
+        for(int j = 0; j < vectorsize; j++)
             A[j] *= 1.1 * 0.9;
-        }
     }
+	auto end = std::chrono::high_resolution_clock::now();
 
-    auto finish = std::chrono::high_resolution_clock::now();
-    std::cout << "Elapsed time: " << (float) std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count()/1000 << " s\n";
-
+	std::cout << std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count() << " seconds" << std::endl;
+	std::cout << omp_get_max_threads() << " threads" <<std::endl;
 }
 
 int main() {
-    #pragma omp parallel for
-    for(int i = 0; i < vectorsize; i++)
-        A[i] = 100.0;
-    cpu();
-    simd();
-    gpu();
 
+	#pragma omp parallel for
+	for(int i = 0; i < vectorsize; i++)
+		A[i] = 100.0;
+
+	cpu();
+	simd();
+	gpu();
+
+	return 0;
 }
